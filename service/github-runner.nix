3ebcae9a763686997800000dcb6091fc4e3b5f20
Isaac Shapira <fresheyeball@protonmail.com>
2021-08-12 22:34:19 -0600

    mutli runner

--- service/github-runner.nix
+++ service/github-runner.nix
@@ -1,16 +1,34 @@
-{pkgs, tokenFile}: {
-  services = {
-    github-runner = {
-      enable = true;
-      name = "Ardana-CI";
-      url = "https://github.com/ArdanaLabs";
-      extraPackages = with pkgs; [
-        stack
-        xz
-        hlint
-        haskellPackages.fourmolu
-      ];
-      inherit tokenFile;
-    };
-  };
+{pkgs, tokenFile}: let token = "/runner-token"; in
+{ containers = builtins.foldl' (acc: num: acc // { ${"Ardana-CI-Container-${num}"} = {
+      autoStart = true;
+      bindMounts.${token} = {
+        hostPath = tokenFile;
+        isReadOnly = true;
+      };
+      config = _:
+        {  services = {
+             github-runner = {
+               enable = true;
+               name = "Ardana-CI-${num}";
+               url = "https://github.com/ArdanaLabs";
+               replace = true;
+               extraPackages = with pkgs; [
+                 bash
+                 coreutils
+                 gnutar
+                 glibc
+                 gzip
+                 git
+                 curl
+                 stack
+                 xz
+                 hlint
+                 haskellPackages.fourmolu
+               ];
+               tokenFile = token;
+             };
+           };
+        };
+      };
+    }) {} (map toString (pkgs.lib.lists.range 0 10));
 }
