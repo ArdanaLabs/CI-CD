740760a2cff5505b04c2de43d6247048e9281303
Isaac Shapira <fresheyeball@protonmail.com>
2021-08-13 11:49:00 -0600

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+source /run/media/isaac/kassir/ardana.keys/env.sh
+nixops deploy -d ardana-ci -I nixpkgs=https://github.com/Fresheyeball/nixpkgs/archive/ef75aabffb4edc75bf4639af8c454eea267fa253.tar.gz
