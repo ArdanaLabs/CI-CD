8a04412a7f6e25027243c2b5c0b063fbadf82c1b
Isaac Shapira <fresheyeball@protonmail.com>
2021-08-17 20:55:04 -0600

    test motd auto deploy

--- box/base.nix
+++ box/base.nix
@@ -92,7 +92,7 @@ in {
       ┃    result in civil and/or criminal penalties. All activities performed on this
       ┃    device are logged and monitored.
       ┃
-      ┃    Platonic.Systems | Ardana Labs
+      ┃    Ardana Labs | Platonic.Systems
       ┖
 
     '';
