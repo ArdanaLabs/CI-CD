019891030636c85f50d76c3c28310021d1b2cb8b
Isaac Shapira <fresheyeball@protonmail.com>
2021-08-17 20:31:52 -0600

    key raw

--- service/github-runner.nix
+++ service/github-runner.nix
@@ -1,49 +1,51 @@
 { pkgs, keys, foldWithKey }:
 { containers = builtins.foldl' (acc: num: acc // { ${"Ardana-CI-Container-${num}"} = {
-      autoStart = true;
-      bindMounts = {
-        "/nix" = {
-          hostPath = "/nix";
-          isReadOnly = false;
+    autoStart = true;
+    bindMounts = {
+      "/nix" = {
+        hostPath = "/nix";
+        isReadOnly = false;
+      };
+    } // foldWithKey (acc: name: key: acc // { "/${name}-raw".hostPath = key.path; }) {} keys;
+    config = _:
+      { services = {
+          github-runner = {
+            enable = true;
+            name = "Ardana-CI-${num}";
+            url = "https://github.com/ArdanaLabs";
+            replace = true;
+            extraPackages = with pkgs; [
+              bash
+              coreutils
+              gnutar
+              glibc
+              gzip
+              git
+              curl
+              stack
+              xz
+              hlint
+              haskellPackages.fourmolu
+              openssh
+              nixops
+              which
+            ];
+            tokenFile = "/github-runner-token";
+          };
+          openssh.knownHosts.${import ../ip.nix}.publicKey =
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJkX4gVJdpVGFYOmdRCj8lgho14DhSEzaViWXYM3em31";
         };
-      } // foldWithKey (acc: name: key: acc // { "/${name}".hostPath = key.path; }) {} keys;
-      config = _:
-        { services = {
-            github-runner = {
-              enable = true;
-              name = "Ardana-CI-${num}";
-              url = "https://github.com/ArdanaLabs";
-              replace = true;
-              extraPackages = with pkgs; [
-                bash
-                coreutils
-                gnutar
-                glibc
-                gzip
-                git
-                curl
-                stack
-                xz
-                hlint
-                haskellPackages.fourmolu
-                openssh
-                nixops
-                which
-              ];
-              tokenFile = "/github-runner-token";
-            };
-            openssh.knownHosts.${import ../ip.nix}.publicKey =
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJkX4gVJdpVGFYOmdRCj8lgho14DhSEzaViWXYM3em31";
-          };
-          systemd.services.build-key-permissions = {
-            serviceConfig.Type = "oneshot";
-            wantedBy = [ "default.target" "github-runner.service" ];
-            script = ''
-              chown github-runner ./build-key
-              chmod 600 ./build-key
-            '';
-          };
+        systemd.services.build-key-permissions = {
+          serviceConfig.Type = "oneshot";
+          wantedBy = [ "default.target" "github-runner.service" ];
+          script = pkgs.lib.concatMapStringsSep "\n" (name:
+          ''
+            cp ./${name}-raw ./${name}
+            chown github-runner ./${name}
+            chmod 600 ./${name}
+          '') (builtins.attrNames keys);
         };
       };
-    }) {} (map toString (pkgs.lib.lists.range 0 10));
+    };
+  }) {} (map toString (pkgs.lib.lists.range 0 10));
 }
