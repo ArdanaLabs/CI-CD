9995489c1202f649fc47f3e1b0fe5d535dfc7d83
Isaac Shapira <fresheyeball@protonmail.com>
2021-08-20 15:15:13 -0600

    hash update

--- local-deploy.sh
+++ local-deploy.sh
@@ -1,2 +1,2 @@
 source /run/media/isaac/kassir/ardana.keys/env.sh
-nixops deploy -d ardana-ci -I nixpkgs=https://github.com/Fresheyeball/nixpkgs/archive/ef75aabffb4edc75bf4639af8c454eea267fa253.tar.gz
+nixops deploy -d ardana-ci -I nixpkgs=https://github.com/Fresheyeball/nixpkgs/archive/060d08cc74919d808cd3608e8391ab12c17cfeb2.tar.gz
