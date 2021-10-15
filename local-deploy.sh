66715ed8fea809ce4a4bfe259a795eaf28e161ac
nixinator <33lockdown33@protonmail.com>
2021-10-15 23:56:59 +0100

    patched local-deploy.sh for nixops deployment key work around

--- local-deploy.sh
+++ local-deploy.sh
@@ -1,3 +1,9 @@
-source /run/media/isaac/kassir/ardana.keys/env.sh
-nixops deploy -d ardana-ci -I nixpkgs=https://github.com/nixinator/nixpkgs/archive/08965379be2dc0623aed11aa4b29c95636fc4f43.tar.gz
+source env.sh
+
+export NIXOPS_STATE=$(mktemp -t tmp.XXXXXXXXX.nixops)
+chmod o-rwx $NIXOPS_STATE
+chmod g-rwx $NIXOPS_STATE
+
+nixops create -d ardana-ci realm.nix 
+nixops deploy -d ardana-ci -I nixpkgs=https://github.com/nixinator/nixpkgs/archive/0866fee90937ccffb3a28565fa5f9e5b49783ff4.tar.gz
 
