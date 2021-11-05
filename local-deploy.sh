fe05c4c5343849638c80cabb5a093a62ec091227
nixinator <33lockdown33@protonmail.com>
2021-11-05 18:06:21 +0000

    github runner to v2-284-0

--- local-deploy.sh
+++ local-deploy.sh
@@ -5,5 +5,5 @@ chmod o-rwx $NIXOPS_STATE
 chmod g-rwx $NIXOPS_STATE
 
 nixops create -d ardana-ci realm.nix 
-nixops deploy -d ardana-ci -I nixpkgs=https://github.com/nixinator/nixpkgs/archive/0866fee90937ccffb3a28565fa5f9e5b49783ff4.tar.gz
+nixops deploy -d ardana-ci -I nixpkgs=https://github.com/nixinator/nixpkgs/archive/07802314390347535970f665e61cc85f1e662e24.tar.gz
 
